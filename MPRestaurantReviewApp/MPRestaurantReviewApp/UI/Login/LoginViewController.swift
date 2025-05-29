f34fd863e0c18b71de7baae492e23954eef9001d
Plamen Atanasov <plmn@plmns-MBA.local>
2025-05-29 17:16:28 +0300

    dev: scroll up login on keyboard appear

--- MPRestaurantReviewApp/MPRestaurantReviewApp/UI/Login/LoginViewController.swift
+++ MPRestaurantReviewApp/MPRestaurantReviewApp/UI/Login/LoginViewController.swift
@@ -12,16 +12,42 @@ final class LoginViewController: UIViewController {
     @IBOutlet private weak var loginButton: UIButton!
     @IBOutlet private weak var signUpButton: UIButton!
     @IBOutlet private weak var usernameTextField: UITextField!
-    @IBOutlet private weak var containerStackView: UIStackView!
+    @IBOutlet private weak var contentScrollView: UIScrollView!
     @IBOutlet private weak var passwordTextField: UITextField!
     
     weak var viewModel: LoginViewModel!
     
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         usernameTextField.delegate = self
         passwordTextField.delegate = self
+        setupButtons()
+        
+        let dismissKeyboardTap = UITapGestureRecognizer(
+            target: self,
+            action: #selector(UIInputViewController.dismissKeyboard)
+        )
+        view.addGestureRecognizer(dismissKeyboardTap)
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillAppear),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillDisappear),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+    
+    // MARK: - UI Setup
+    private func setupButtons() {
         loginButton.layer.cornerRadius = 10
         
         let signUpBtnAttributes: [NSAttributedString.Key: Any] = [
@@ -36,22 +62,9 @@ final class LoginViewController: UIViewController {
         )
         
         signUpButton.setAttributedTitle(attributeString, for: .normal)
-        
-//        let guide = view.keyboardLayoutGuide
-//        guide.followsUndockedKeyboard = true
-//        
-//        containerStackView.translatesAutoresizingMaskIntoConstraints = false
-//        
-//        NSLayoutConstraint.activate([
-//            containerStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-//            containerStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-//            containerStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
-//            containerStackView.bottomAnchor.constraint(equalTo: guide.topAnchor, constant: -12)
-//        ])
-//        
-//        
     }
     
+    // MARK: - IBActions
     @IBAction private func didTapLoginButton(_ sender: Any) {
         print("LOGIN")
     }
@@ -59,11 +72,29 @@ final class LoginViewController: UIViewController {
     @IBAction private func didTapSignUpButton(_ sender: Any) {
         print("SIGN UP")
     }
+    
+    // MARK: - Keyboard Management
+    @objc private func keyboardWillAppear() {
+        guard contentScrollView.contentOffset.y == 0 else { return }
+        contentScrollView.contentOffset.y += 100
+    }
+    
+    @objc private func keyboardWillDisappear() {
+        contentScrollView.contentOffset.y = 0
+    }
+    
+    @objc private func dismissKeyboard() {
+        view.endEditing(true)
+    }
 }
 
 extension LoginViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
     }
 }
